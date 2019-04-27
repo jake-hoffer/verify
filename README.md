0db73f7cfc72acf33ff0762336d46e9df8c87e05
jake-hoffer <hoffer.188@gmail.com>
2019-04-27 00:36:02 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# Verify
+A [very] lightweight JS testing framework for node
+
+## Background
+
+As I am new to the world of TDD, I found I fatigued rather quickly of the enormous and complex libraries required just to test my code. So I decided to develop my own that is much more compact, though perhaps lacking some of the less common features available in typical testing frameworks.
+
+## Usage
+
+Simply require verify.js at the top of your testing script. Each time you write a test, you will call `verify()` with the first argument being the test description, and the second argument being an anonymous function. The function chained to `verify()` will evaluate its return, output, and error and compare it to what is expected. These are the available functions for evaluation:
+
+- `returns()`: verify that the return value of the function is equal to the given value
+	```javascript
+	verify(function() { return true; }).returns(true); // PASS
+	verify(function() { return false; }).returns(true); // FAIL
+	```
+- `prints()`: verify that the printed output of the function (sans trailing newline) is equal to the given value
+	```javascript
+	verify(function() { console.log("foo"); }).prints("foo"); // PASS
+	verify(function() { console.log("foo"); }).prints("bar"); // FAIL
+	```
+- `returnsInstanceOf()`: verify that the return value of the function is an instance of the given class/object
+	```javascript
+	verify(function() { return /foo/; }).returnsInstanceOf(RegExp); // PASS
+	verify(function() { return /foo/; }).returnsInstanceOf(Date); // FAIL
+	```
+- `throwsError()`: verify that the function throws an error; if an argument is given, verify that it is equal to the error thrown
+	```javascript
+	verify(function() { new RegExp('[a-z+'); }).throwsError(); // PASS
+	verify(function() { new RegExp('[a-z]+'); }).throwsError(); // FAIL
+	```
+
+A standalone `tally()` function is also provided, which prints the cumulative score of the tests.
---
+++ verify.js
@@ -0,0 +1,143 @@
+function isIdentical(a, b) {
+	if (typeof a !== typeof b) { return false; }
+	if (a === null) { return a === b; }
+	if (typeof a !== 'object') { return a === b; }
+
+	if (Object.getPrototypeOf(a).constructor && Object.getPrototypeOf(a).constructor.name && ['Error', 'RegExp'].includes(Object.getPrototypeOf(a).constructor.name) && Object.getPrototypeOf(b).constructor && Object.getPrototypeOf(b).constructor.name && ['Error', 'RegExp'].includes(Object.getPrototypeOf(b).constructor.name)) {
+		return a.toString() == b.toString();
+	}
+
+	if (Array.isArray(a) != Array.isArray(b)) { return false; }
+	if (a.length !== b.length) { return false; }
+
+	if (Array.isArray(a)) {
+		for (let i = 0, l = a.length; i < l; i++) {
+			if (!isIdentical(a[i], b[i])) {
+				return false;
+			}
+		}
+		return true;
+	}
+	else if (Object.prototype.toString.call(b) === '[object Object]') {
+		for (i in a) {
+			if (!(b.hasOwnProperty(i) ? isIdentical(a[i], b[i]) : false)) {
+				return false;
+			}
+		}
+		return true;
+	}
+	else {
+		return false;
+	}
+	return false;
+}
+
+let TALLY = { PASS: 0, FAIL: 0, NULL: 0 };
+
+function verify(desc, func) {
+	var RET = null;
+	var ERR = null;
+	var OUT = null;
+	var V = {};
+
+	var desc = typeof desc === 'undefined' ? null : desc;
+	if (typeof desc === 'function' && typeof func === 'undefined') {
+		func = desc;
+		desc = "\x1b[36m" + func.toString().replace(/^.*\n[ \t]*/, '').replace(/[ \t]*\n.*$/, '').replace(/^.*?return +/, '').replace(/ *;.*$/, '') + "\x1b[0m";
+	}
+
+	if (typeof func === 'function') {
+		let oldWrite = process.stdout.write;
+
+		process.stdout.write = (function(write) {
+			return function (string, encoding, fd) {
+				OUT = OUT === null ? string : OUT + string;
+			}
+		})(process.stdout.write);
+
+		try {
+			RET = func.apply(this);
+		}
+		catch (err) {
+			ERR = err;
+		}
+
+		process.stdout.write = oldWrite;
+
+		OUT = OUT !== null ? OUT.replace(/\n$/, '') : null;
+	}
+
+	var _score = function(passed, shouldTxt, shouldVal) {
+		if (typeof func !== 'function') {
+			TALLY.NULL++;
+			console.log("\x1b[1m\x1b[33mNULL\x1b[0m", ":", "\x1b[36mNothing was evaluated\x1b[0m");
+			return null;
+		}
+
+		
+		if (passed) {
+			TALLY.PASS++;
+			console.log("\x1b[1m\x1b[32mPASS\x1b[0m", ":", desc);
+		}
+		else {
+			TALLY.FAIL++;
+			console.log("\x1b[1m\x1b[31mFAIL\x1b[0m", ":", desc);
+			console.log("       this should " + shouldTxt, "\t", shouldVal);
+			instead = false;
+			if (RET) {
+				if (typeof RET === 'object' && RET !== null && Object.getPrototypeOf(RET).constructor && Object.getPrototypeOf(RET).constructor.name && !['Array', 'Object', 'Error', 'RegExp'].includes(Object.getPrototypeOf(RET).constructor.name)) {
+					RET = '(instance of) ' + Object.getPrototypeOf(RET).constructor.name;
+				}
+				console.log("       " + (instead ? "            " : "instead, it ") + "returned:", "\t", RET);
+				instead = true;
+			}
+			if (OUT) {
+				console.log("       " + (instead ? "            " : "instead, it ") + "printed:", "\t", OUT);
+				instead = true;
+			}
+			if (ERR) {
+				console.log("       " + (instead ? "            " : "instead, it ") + "threw the error:", "\t", ERR);
+				instead = true;
+			}
+		}
+		return passed;
+	}
+	
+	V.returns = function(expected) {
+		return _score(isIdentical(RET, expected), "return:", expected);
+	}
+	
+	V.prints = function(expected) {
+		return _score(isIdentical(OUT, expected), "print:", expected);
+	}
+	
+	V.returnsInstanceOf = function(expected) {
+		return _score(RET instanceof expected, "return:", "(instance of) " + expected.name);
+	}
+	
+	V.throwsError = function(expected) {
+		if (typeof expected !== 'undefined') {
+			return _score(isIdentical(ERR, expected), "throw the error:", expected.name);
+		}
+		else {
+			return _score(ERR !== null, "throw an error", "");
+		}
+	}
+	
+	V.RET = RET;
+	V.OUT = OUT;
+	V.ERR = ERR;
+	
+	return V;
+}
+	
+function tally() {
+	let total = TALLY.PASS + TALLY.FAIL + TALLY.NULL;
+	console.log("OUT OF " + total + " TEST" + (total == 1 ? "" : "S") + ":", [(TALLY.PASS ? "\x1b[1m\x1b[32m" + TALLY.PASS + " PASSED\x1b[0m" : ""), (TALLY.FAIL ? "\x1b[1m\x1b[31m" + TALLY.FAIL + " FAILED\x1b[0m" : ""), (TALLY.NULL ? "\x1b[1m\x1b[33m" + TALLY.NULL + " NULL\x1b[0m" : "")].filter(type => type != "").join(" | "));
+}
+
+module.exports = function() {
+	// this.isIdentical = isIdentical;
+	this.verify = verify;
+	this.tally = tally;
+}
